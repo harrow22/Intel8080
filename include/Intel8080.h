e4ce039c2dd07fe1ad3178ff8ff96f4a0612f7d2
kamal <kamal.ali.02357@gmail.com>
2023-09-21 15:50:05 -0400

    data transfer group

--- include/Intel8080.h
+++ include/Intel8080.h
@@ -2,18 +2,16 @@
 #define INTEL8080_INTEL8080_H
 
 #include <cstdint>
-#include <vector>
-#include "../src/Registers.h"
 
 /*
  * Intel 8080 Emulated Pinout:
  *             ┌───────────────────┐
  *    A0 ○<————│  1             34 │<————○ READY
  *    A1 ○<————│  2             33 │<————○ INT
- *    A2 ○<————│  3             32 │<————○ HOLD
- *    A3 ○<————│  4             31 │<————○ RESET
+ *    A2 ○<————│  3             32 │<————○ HOLD ** REMOVE
+ *    A3 ○<————│  4             31 │<————○ RESET ** REMOVE implemented as a function
  *    A4 ○<————│  5             30 │————>○ WAIT
- *    A5 ○<————│  6    INTEL    29 │————>○ HLDA
+ *    A5 ○<————│  6    INTEL    29 │————>○ HLDA ** REMOVE
  *    A6 ○<————│  7     8080    28 │————>○ SYNC
  *    A7 ○<————│  8             27 │————>○ WR
  *    A8 ○<————│  9             26 │————>○ DBIN
@@ -27,37 +25,65 @@
  *    D0 ○<———>│ 17             18 │<———>○ D1
  *             └───────────────────┘
  *
- * Status Information Definition:
- * --------------------------------------------------------------------------------------------------------------
+ * ## Member Functions:
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ * void tick();
+ *   - Steps the processor one state forward. Each instruction consists of 1-5 machine cycles and 3-5 states (T1-T5)
+ *   - constitute a machine cycle. A full instruction cycle requires anywhere from 4-18 states for its completion.
+ *
+ * void reset();
+ *   - Restores the processor's internal program counter to zero, and the cpu will begin the next cycle from T1. Note,
+ *   - however, that this has no effect on status flags, or on any of the processor's working registers
+ *
+ * const std::uint_fast64_t getABus();
+ *    - Returns the current bits on the address bus.
+ *
+ * const std::uint_fast64_t getDBus();
+ *    - Returns the current bits on the data bus.
+ *
+ *
+ * ## Member Variables:
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ * std::uint8_t status;
+ *    - Identifies the current machine cycle. Is set in the second state of every machine cycle.
+ *
+ * std::uint_fast_64_t pins;
+ *    - The current state of the processors pins.
+ *
+ *
+ * ## Status Information Definition:
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
  *          Data Bus
  * Symbols     Bit                                      Definition
- * INTA        D0  -  Acknowledge signal for INTERRUPT request. Signal should be used to gate a restart
- *                    instruction onto the data bus when DBIN is active.
+ * INTA        D0  -  Acknowledge signal for INTERRUPT request. Signal should be used to gate a restart instruction onto
+ *                    the data bus when DBIN is active.
  * WO          D1  -  Indicates that the operation in the current machine cycle will be a WRITE memory or OUTPUT
  *                    function (WO = 0). Otherwise a READ memory or INPUT operation will be executed.
  * STACK       D2  -  Indicates that the address bus hold the pushdown stack address from the Stack Pointer.
  * HLTA        D3  -  Acknowledge signal for HALT instruction.
- * OUT         D4  -  Indicates that the address bus contains the address of an output device and the data bus
- *                    will contain the output data when WR is active.
+ * OUT         D4  -  Indicates that the address bus contains the address of an output device and the data bus will
+ *                    contain the output data when WR is active.
  * M1          D5  -  Provides a signal to indicate that the CPU is in the fetch cycle for the first byte of an
  *                    instruction.
- * INP         D6  -  Indicates that the address bus contains the address of an input device and the input data
- *                    should be placed on the data bus when DBIN is active.
+ * INP         D6  -  Indicates that the address bus contains the address of an input device and the input data should
+ *                    be placed on the data bus when DBIN is active.
  * MEMR        D7  -  Designates that the data bus will be used for memory read data.
- * --------------------------------------------------------------------------------------------------------------
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
  * *Data bus bit constants are named as their respective symbols (instead of D0-D7) for readability.
  *
- * Control Output Definition:
- * --------------------------------------------------------------------------------------------------------------
+ *
+ * ## Machine Cycle Identification:
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ *
+ *
+ *
+ * ## Control Output Definition:
+ * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
  * SYNC  -  Marks the beginning of every machine cycle. Active from T1 until T2.
- *
- *
  */
 
 class Intel8080 {
 public:
-    void tick();
-
     // pin bit constants
     static constexpr std::uint_fast64_t A0 {1ULL << 0};
     static constexpr std::uint_fast64_t A1 {1ULL << 1};
@@ -98,40 +124,64 @@ public:
     static constexpr std::uint_fast64_t abus {0xFFFFULL};
     static constexpr std::uint_fast64_t dbus {0xFF0000ULL};
 
+    void tick();
+    void reset() { pc_ = step_ = 0; }
+    [[nodiscard]] std::uint_fast64_t getABus() const { return pins & abus; }
+    [[nodiscard]] std::uint_fast64_t getDBus() const { return pins & dbus; }
+
     std::uint8_t status {0};
     std::uint_fast64_t pins {0};
-
-    [[nodiscard]] auto getABus()->std::uint_fast64_t { return pins & abus; }
-    [[nodiscard]] auto getDBus()->std::uint_fast64_t { return pins & dbus; }
 private:
-    static constexpr std::uint8_t signBit {0x80};
-    static constexpr std::uint8_t zeroBit {0x40};
-    static constexpr std::uint8_t auxiliaryBit {0x10};
-    static constexpr std::uint8_t parityBit {0x04};
-    static constexpr std::uint8_t carryBit {0x01};
+    // flag bit constants
+    static constexpr std::uint8_t signBit {0x80U};
+    static constexpr std::uint8_t zeroBit {0x40U};
+    static constexpr std::uint8_t auxiliaryBit {0x10U};
+    static constexpr std::uint8_t parityBit {0x04U};
+    static constexpr std::uint8_t carryBit {0x01U};
 
-    static constexpr std::uint8_t BC {0};
-    static constexpr std::uint8_t DE {2};
-    static constexpr std::uint8_t HL {4};
-    static constexpr std::uint8_t A {7};
-    static constexpr std::uint8_t F {8};
-
+    // register name constants
+    static constexpr std::uint8_t B {0U};
+    static constexpr std::uint8_t C {1U};
+    static constexpr std::uint8_t D {2U};
+    static constexpr std::uint8_t E {3U};
+    static constexpr std::uint8_t H {4U};
+    static constexpr std::uint8_t L {5U};
+    static constexpr std::uint8_t W {9U};
+    static constexpr std::uint8_t Z {10U};
+    static constexpr std::uint8_t BC {B};
+    static constexpr std::uint8_t DE {D};
+    static constexpr std::uint8_t HL {H};
+    static constexpr std::uint8_t WZ {W};
+    static constexpr std::uint8_t A {7U};
+    static constexpr std::uint8_t F {8U};
 
     void setDBus(std::uint_fast64_t val) { pins = pins & ~0xFF0000ULL | val<<16ULL; }
-    void setABus(std::uint_fast64_t val) { pins = pins & ~0xFF | val; }
+    void setABus(std::uint_fast64_t val) { pins = pins & ~0xFFULL | val; }
 
-    void t2_();
-    void readT1_(std::uint16_t addr) { pins |= SYNC; setDBus(WO|M1|MEMR); setABus(addr); }
+    void t2_()
+    {
+        if (pins & HOLD) {
+
+        }
+        status = getDBus();
+        pins &= ~SYNC;
+        if (!(pins & READY))
+            pins |= WAIT;
+    };
+    void fetch_() { pins |= SYNC; setDBus(WO|M1|MEMR); setABus(pc_); }
+    void readT1_(std::uint16_t addr) { pins |= SYNC; setDBus(WO|MEMR); setABus(addr); }
     void readT2_() { t2_(); pins |= DBIN; }
     void writeT1_(std::uint16_t addr) { pins |= SYNC; setDBus(0); setABus(addr); }
     void writeT2_(std::uint8_t r) { t2_(); pins |= DBIN; setDBus(r); }
     void stopDataIn() { pins &= ~DBIN; }
 
     [[nodiscard]] bool waiting_() const { return pins & WAIT; }
-    std::uint16_t pair_(std::uint8_t rp) { return reg_[rp] << 8 | reg_[rp+1]; }
-    void transfer8_(std::uint8_t& dst) { dst = getDBus(); }
-    [[nodiscard]] std::uint8_t& dst_() { return reg_[ir_ >> 3 & 7]; }
-    [[nodiscard]] std::uint8_t& src_() { return reg_[ir_ & 7]; }
+    std::uint16_t pair_(std::uint8_t rp) { return reg_[rp] << 8U | reg_[rp+1]; }
+    void transfer8_(std::uint8_t& dst) const { dst = getDBus(); }
+    void transfer16_(std::uint8_t rp) { reg_[rp << 1] = getDBus(); }
+    [[nodiscard]] std::uint8_t rp_() const { return ir_ >> 4U & 3U; }
+    [[nodiscard]] std::uint8_t dst_() const { return ir_ >> 3U & 7U; }
+    [[nodiscard]] std::uint8_t src_() const { return ir_ & 7U; }
 
     void setSignFlag(bool enabled) { enabled ? reg_[F] |= signBit : reg_[F] &= ~signBit; }
     void setZeroFlag(bool enabled) { enabled ? reg_[F] |= zeroBit : reg_[F] &= ~zeroBit; }
@@ -139,18 +189,11 @@ private:
     void setParityFlag(bool enabled) { enabled ? reg_[F] |= parityBit : reg_[F] &= ~parityBit; }
     void setCarryFlag(bool enabled) { enabled ? reg_[F] |= carryBit : reg_[F] &= ~carryBit; }
 
+    // registers
     std::uint16_t pc_ {0}, sp_ {0};
-    std::uint8_t ir_ {};
-    std::uint8_t reg_[9] {};
+    std::uint8_t ir_ {0}, tmp_ {0};
+    std::uint8_t reg_[11] {};
     std::uint16_t step_ {0};
 };
-
-void Intel8080::t2_()
-{
-    status = getDBus();
-    pins &= ~SYNC;
-    if (!(pins & READY))
-        pins |= WAIT;
-}
 
 #endif //INTEL8080_INTEL8080_H
--- src/Intel8080.cpp
+++ src/Intel8080.cpp
@@ -3,97 +3,97 @@
 #include <vector>
 
 constexpr int mnemonic[72] {
-    3, // MOV r1, r2
-    5, // MOV r, M
-    8, // MOV M, r
-    0, // SPHL
-    0, // MVI r, data
-    0, // MVI M, data
-    0, // LXI rp, data
-    0, // LDA addr
-    0, // STA addr
-    0, // LHLD addr
-    0, // SHLD addr
-    0, // LDAX rp
-    0, // STAX rp
-    0, // XCHG
-    0, // ADD r
-    0, // ADD M
-    0, // ADI data
-    0, // ADC r
-    0, // ADC M
-    0, // ACI data
-    0, // SUB r
-    0, // SUB M
-    0, // SUI data
-    0, // SBB r
-    0, // SBB M
-    0, // SBI data
-    0, // INR r
-    0, // INR M
-    0, // DCR r
-    0, // DCR M
-    0, // INX rp
-    0, // DCX rp
-    0, // DAD rp
-    0, // DAA
-    0, // ANA r
-    0, // ANA M
-    0, // ANI data
-    0, // XRA r
-    0, // XRA M
-    0, // XRI data
-    0, // ORA r
-    0, // ORA M
-    0, // ORI data
-    0, // CMP r
-    0, // CMP M
-    0, // CPI data
-    0, // RLC
-    0, // RRC
-    0, // RAL
-    0, // RAR
-    0, // CMA
-    0, // CMC
-    0, // STC
-    0, // JMP addr
-    0, // J cond addr
-    0, // CALL addr
-    0, // C cond addr
-    0, // RET
-    0, // R cond addr
-    0, // RST n
-    0, // PCHL
-    0, // PUSH rp
-    0, // PUSH PSW
-    0, // POP rp
-    0, // POP PSW
-    0, // XTHL
-    0, // IN port
-    0, // OUT port
-    0, // EI
-    0, // DI
-    0, // HLT
-    0, // NOP
+        3, // MOV r1, r2
+        5, // MOV r, M
+        8, // MOV M, r
+        11, // SPHL
+        13, // MVI r, data
+        18, // MVI M, data
+        25, // LXI rp, data
+        32, // LDA addr
+        42, // STA addr
+        52, // LHLD addr
+        66, // SHLD addr
+        79, // LDAX rp
+        83, // STAX rp
+        87, // XCHG
+        0, // ADD r
+        0, // ADD M
+        0, // ADI data
+        0, // ADC r
+        0, // ADC M
+        0, // ACI data
+        0, // SUB r
+        0, // SUB M
+        0, // SUI data
+        0, // SBB r
+        0, // SBB M
+        0, // SBI data
+        0, // INR r
+        0, // INR M
+        0, // DCR r
+        0, // DCR M
+        0, // INX rp
+        0, // DCX rp
+        0, // DAD rp
+        0, // DAA
+        0, // ANA r
+        0, // ANA M
+        0, // ANI data
+        0, // XRA r
+        0, // XRA M
+        0, // XRI data
+        0, // ORA r
+        0, // ORA M
+        0, // ORI data
+        0, // CMP r
+        0, // CMP M
+        0, // CPI data
+        0, // RLC
+        0, // RRC
+        0, // RAL
+        0, // RAR
+        0, // CMA
+        0, // CMC
+        0, // STC
+        0, // JMP addr
+        0, // J cond addr
+        0, // CALL addr
+        0, // C cond addr
+        0, // RET
+        0, // R cond addr
+        0, // RST n
+        0, // PCHL
+        0, // PUSH rp
+        0, // PUSH PSW
+        0, // POP rp
+        0, // POP PSW
+        0, // XTHL
+        0, // IN port
+        0, // OUT port
+        0, // EI
+        0, // DI
+        0, // HLT
+        0, // NOP
 };
 
 constexpr int opcode[256] {
-    71, 6, 12, 30, 26, 28, 4, 46, 71, 32, 11, 31, 26, 28, 4, 47,
-    71, 6, 12, 30, 26, 28, 4, 48, 71, 32, 11, 31, 26, 28, 4, 49,
-    71, 6, 10, 30, 26, 28, 4, 33, 71, 32, 9, 31, 26, 28, 4, 50,
-    71, 6, 8, 30, 27, 29, 5, 52, 71, 32, 7, 31, 26, 28, 4, 51,
-    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-    2, 2, 2, 2, 2, 2, 70, 2, 0, 0, 0, 0, 0, 0, 1, 0,
-    14, 14, 14, 14, 14, 14, 15, 14, 17, 17, 17, 17, 17, 17, 18, 17,
-    20, 20, 20, 20, 20, 20, 21, 20, 23, 23, 23, 23, 23, 23, 24, 23,
-    34, 34, 34, 34, 34, 34, 35, 34, 37, 37, 37, 37, 37, 37, 38, 37,
-    40, 40, 40, 40, 40, 40, 41, 40, 43, 43, 43, 43, 43, 43, 44, 43,
-    58, 63, 54, 53, 56, 61, 16, 59, 58, 57, 54, 53, 56, 55, 19, 59,
-    58, 63, 54, 67, 56, 61, 22, 59, 58, 57, 54, 66, 56, 55, 25, 59,
-    58, 63, 54, 65, 56, 61, 36, 59, 58, 60, 54, 13, 56, 55, 39, 59,
-    58, 64, 54, 69, 56, 62, 42, 59, 58, 3, 54, 68, 56, 55, 45, 59,
+        71, 6, 12, 30, 26, 28, 4, 46, 71, 32, 11, 31, 26, 28, 4, 47,
+        71, 6, 12, 30, 26, 28, 4, 48, 71, 32, 11, 31, 26, 28, 4, 49,
+        71, 6, 10, 30, 26, 28, 4, 33, 71, 32, 9, 31, 26, 28, 4, 50,
+        71, 6, 8, 30, 27, 29, 5, 52, 71, 32, 7, 31, 26, 28, 4, 51,
+        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+        2, 2, 2, 2, 2, 2, 70, 2, 0, 0, 0, 0, 0, 0, 1, 0,
+        14, 14, 14, 14, 14, 14, 15, 14, 17, 17, 17, 17, 17, 17, 18, 17,
+        20, 20, 20, 20, 20, 20, 21, 20, 23, 23, 23, 23, 23, 23, 24, 23,
+        34, 34, 34, 34, 34, 34, 35, 34, 37, 37, 37, 37, 37, 37, 38, 37,
+        40, 40, 40, 40, 40, 40, 41, 40, 43, 43, 43, 43, 43, 43, 44, 43,
+        58, 63, 54, 53, 56, 61, 16, 59, 58, 57, 54, 53, 56, 55, 19, 59,
+        58, 63, 54, 67, 56, 61, 22, 59, 58, 57, 54, 66, 56, 55, 25, 59,
+        58, 63, 54, 65, 56, 61, 36, 59, 58, 60, 54, 13, 56, 55, 39, 59,
+        58, 64, 54, 69, 56, 62, 42, 59, 58, 3, 54, 68, 56, 55, 45, 59,
 };
 
 /**
@@ -107,11 +107,11 @@ void Intel8080::tick()
     switch (step_) {
         // instruction fetch
         case 0:
-            readT1_(pc_);
+            fetch_();
             goto next;
         case 1:
             readT2_();
-            pc_ += 1;
+            ++pc_;
             goto next;
         case 2:
             if (waiting_()) goto wait;
@@ -122,13 +122,13 @@ void Intel8080::tick()
                 break;
             }
 
-        // MOV r1, r2
+            // MOV r1, r2
         case 3: goto next;
         case 4:
-            dst_() = src_();
+            reg_[dst_()] = reg_[src_()];
             goto done;
 
-        // MOV r, M
+            // MOV r, M
         case 5:
             readT1_(pair_(HL));
             goto next;
@@ -139,16 +139,16 @@ void Intel8080::tick()
             if (waiting_()) goto wait;
             else {
                 stopDataIn();
-                transfer8_(dst_());
+                transfer8_(reg_[dst_()]);
                 goto done;
             }
 
-        // MOV M, r
+            // MOV M, r
         case 8:
             writeT1_(pair_(HL));
             goto next;
         case 9:
-            writeT2_(dst_());
+            writeT2_(reg_[dst_()]);
             goto next;
         case 10:
             if (waiting_()) goto wait;
@@ -157,21 +157,330 @@ void Intel8080::tick()
                 goto done;
             }
 
-        // SPHL
-        case 11:goto next;
+            // SPHL
+        case 11: goto next;
         case 12:
             sp_ = pair_(HL);
             goto done;
 
-        // MVI r, data
+            // MVI r, data
+        case 13: goto next;
+        case 14:
+            readT1_(pc_);
+            goto next;
+        case 16:
+            readT2_();
+            goto next;
+        case 17:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[dst_()]);
+                goto done;
+            }
 
+            // MVI M, data
+        case 18: goto next;
+        case 19:
+            readT1_(pc_);
+            goto next;
+        case 20:
+            readT2_();
+            goto next;
+        case 21:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(tmp_);
+                goto next;
+            }
+        case 22:
+            writeT1_(pair_(HL));
+            goto next;
+        case 23:
+            writeT2_(tmp_);
+            goto next;
+        case 24:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                goto done;
+            }
+
+            // LXI rp, data
+        case 25: goto next;
+        case 26:
+            readT1_(pc_);
+            goto next;
+        case 27:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 28:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer16_(rp_() + 1U);
+                goto next;
+            }
+        case 29:
+            readT1_(pc_);
+            goto next;
+        case 30:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 31:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer16_(rp_());
+                goto done;
+            }
+
+        // LDA addr
+        case 32: goto next;
+        case 33:
+            readT1_(pc_);
+            goto next;
+        case 34:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 35:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[Z]);
+                goto next;
+            }
+        case 36:
+            readT1_(pc_);
+            goto next;
+        case 37:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 38:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[W]);
+                goto next;
+            }
+        case 39:
+            readT1_(pair_(WZ));
+            goto next;
+        case 40:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 41:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[A]);
+                goto done;
+            }
+
+        // STA addr
+        case 42: goto next;
+        case 43:
+            readT1_(pc_);
+            goto next;
+        case 44:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 45:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[Z]);
+                goto next;
+            }
+        case 46:
+            readT1_(pc_);
+            goto next;
+        case 47:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 48:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[W]);
+                goto next;
+            }
+        case 49:
+            writeT1_(pair_(W));
+            goto next;
+        case 50:
+            writeT2_(reg_[A]);
+            goto next;
+        case 51:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                goto done;
+            };
+
+        // LHLD addr
+        case 52: goto next;
+        case 53:
+            readT1_(pc_);
+            goto next;
+        case 54:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 56:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[Z]);
+                goto next;
+            }
+        case 57:
+            readT1_(pc_);
+            goto next;
+        case 58:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 59:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[W]);
+                goto next;
+            }
+        case 60:
+            readT1_(pair_(WZ) + 1U);
+            goto next;
+        case 61:
+            readT2_();
+            goto next;
+        case 62:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[L]);
+                goto next;
+            }
+        case 63:
+            readT1_(pair_(WZ));
+            goto next;
+        case 64:
+            readT2_();
+            goto next;
+        case 65:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[H]);
+                goto done;
+            }
+
+        // SHLD addr
+        case 66: goto next;
+        case 67:
+            readT1_(pc_);
+            goto next;
+        case 68:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 69:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[Z]);
+                goto next;
+            }
+        case 70:
+            readT1_(pc_);
+            goto next;
+        case 71:
+            readT2_();
+            ++pc_;
+            goto next;
+        case 72:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[W]);
+                goto next;
+            }
+        case 73:
+            writeT1_(pair_(WZ) + 1U);
+            goto next;
+        case 74:
+            writeT2_(reg_[L]);
+            goto next;
+        case 75:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                goto next;
+            }
+        case 76:
+            writeT1_(pair_(WZ));
+            goto next;
+        case 77:
+            writeT2_(reg_[H]);
+            goto next;
+        case 78:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                goto done;
+            }
+
+        // LDAX rp
+        case 79: goto next;
+        case 80:
+            readT1_(pair_(rp_()));
+            goto next;
+        case 81:
+            readT2_();
+            goto next;
+        case 82:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                transfer8_(reg_[A]);
+                goto done;
+            }
+
+        // STAX rp
+        case 83: goto next;
+        case 84:
+            writeT1_(pair_(rp_()));
+            goto next;
+        case 85:
+            writeT2_(reg_[A]);
+            goto next;
+        case 86:
+            if (waiting_()) goto wait;
+            else {
+                stopDataIn();
+                goto done;
+            }
+
+        // XCHG
+        case 87:
+            std::swap(reg_[H], reg_[D]);
+            std::swap(reg_[L], reg_[E]);
+            goto done;
     }
 
     wait:
-        if (pins & READY)
-            pins &= ~WAIT;
+    if (pins & READY)
+        pins &= ~WAIT;
     next:
-        ++step_;
+    ++step_;
     done:
-        step_ = 0;
+    step_ = 0;
 }
